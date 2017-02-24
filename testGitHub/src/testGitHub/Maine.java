2543561e77eac417f458d98ca3e51e72bb637ca2
Nazariy Dykiy <ndykiy@gmail.com>
2017-02-24 17:03:19 +0200

    replace with previous commit

--- testGitHub/src/testGitHub/Maine.java
+++ testGitHub/src/testGitHub/Maine.java
@@ -3,9 +3,7 @@ package testGitHub;
 public class Maine {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hi");
-
+System.out.println("hi");
 	}
 
 }
